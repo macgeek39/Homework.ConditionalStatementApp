2e6c0b56b8455d5defdc5094d54acac7dc4134b2
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-02 04:16:46 -0600

    Finished Homework

--- HomeworkConditionalStatement/Program.cs
+++ HomeworkConditionalStatement/Program.cs
@@ -1,3 +1,29 @@
 ﻿
 
+/*
+ * Create a Console Application that asks
+ * the user for their name. Welcome Tim as professor
+ * or anyone else as student. Make sure that "TIM"
+ * also gets called professor.
+ */
 
+
+// Ask user for their name
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+// Conver string to lower case
+string converName = firstName.ToLower();
+
+// If name is tim
+// Welcome as professor
+// Or else as student
+
+if (converName == "tim")
+{
+    Console.WriteLine("Welcome Professor");
+}
+else
+{
+    Console.WriteLine("What's up Student");
+}
